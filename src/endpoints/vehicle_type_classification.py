7e645179a60fc588bd4ac4903c35dbdbb3dc8582
Alan Churichi <achurichi@gmail.com>
2021-08-01 14:37:13 -0300

    Removes pillow

--- src/endpoints/vehicle_type_classification.py
+++ src/endpoints/vehicle_type_classification.py
@@ -1,16 +1,15 @@
 from flask import Blueprint, Response
 from flask import request, jsonify
 import os
+import cv2 
 import numpy as np
 from keras import models
-from keras.preprocessing.image import load_img, img_to_array
 
 api_version = os.environ['API_VERSION']
 input_shape = int(os.environ['VTC_INPUT_SHAPE'])
 model_name = os.environ['VTC_MODEL_NAME']
 models_path = '/root/app/models'
 images_path = '/root/app/images'
-vtc_url = '/api/' + api_version + '/vtc'
 decoder = {
     0: 'car',
     1: 'motorbike',
@@ -20,32 +19,27 @@ decoder = {
 model = models.load_model(os.path.join(models_path, model_name))
 
 # Make a prediction to load cache and save time on following requests
-image = load_img(os.path.join(images_path, 'test-0001.jpg'),
-                 target_size=(input_shape, input_shape))
-image = img_to_array(image)
-image = image.reshape(
-    (1, image.shape[0], image.shape[1], image.shape[2]))
+image = cv2.imread('/root/app/test_image/test.jpg')
+image = cv2.resize(image, dsize=(input_shape, input_shape), interpolation=cv2.INTER_NEAREST)
+image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
 model.predict(image)
 
-vtc = Blueprint('vtc', __name__, template_folder='templates')
+vtc = Blueprint('vtc', __name__)
 
 
-@vtc.route(f'{vtc_url}/predict', methods=['GET'])
+@vtc.route(f'/api/{api_version}/vtc/predict', methods=['GET'])
 def vtc_predict():
     try:
         image_filename = request.json['filename']
     except:
         return Response('Wrong request body', status=400)
 
-    try:
-        image = load_img(os.path.join(images_path, image_filename),
-                         target_size=(input_shape, input_shape))
-    except:
+    image = cv2.imread(os.path.join(images_path, image_filename))
+    if image is None:
         return Response('Image file not found', status=500)
 
-    image = img_to_array(image)
-    image = image.reshape(
-        (1, image.shape[0], image.shape[1], image.shape[2]))
+    image = cv2.resize(image, dsize=(input_shape, input_shape), interpolation=cv2.INTER_NEAREST)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
 
     prediction = model.predict(image)
 
@@ -54,8 +48,8 @@ def vtc_predict():
     prediction_score = np.amax(prediction)
 
     result = {
-        'label': prediction_label,
-        'score': str(prediction_score)
+        'type': prediction_label,
+        'score': float(prediction_score)
     }
 
     return jsonify(result)
